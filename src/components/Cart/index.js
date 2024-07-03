3ab8f9edae61a368235d0d347c91e114886b534f
Naveenp999 <naveenpillalamarry999@gmail.com>
2024-07-03 09:45:37 +0000

    autentication-2

---
+++ src/components/Cart/index.js
@@ -0,0 +1,16 @@
+import './index.css'
+
+import Header from '../Header'
+
+const Cart = () => (
+  <div className="not-found-container">
+    <Header />
+    <img
+      src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-cart-img.png"
+      alt="cart"
+      className="all-img"
+    />
+  </div>
+)
+
+export default Cart
---
+++ src/components/Products/index.js
@@ -0,0 +1,18 @@
+import './index.css'
+
+import Header from '../Header'
+
+const Products = () => (
+  <>
+    <Header />
+    <div className="not-found-container">
+      <img
+        src="https://assets.ccbp.in/frontend/react-js/nxt-trendz-products-img.png"
+        alt="products"
+        className="all-img"
+      />
+    </div>
+  </>
+)
+
+export default Products
